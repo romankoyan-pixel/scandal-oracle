7a45e7e2d1e294e9ed13d3256d8033cd15317627
romankoyan-pixel <romankoyan@gmail.com>
2025-12-26 11:05:25 +0300

    fix: restore gitignore and finalize deployment prep

---
+++ config/feeds.js
@@ -0,0 +1,76 @@
+// RSS Feed Sources - Trusted Worldwide
+const RSS_FEEDS = {
+    politics: [
+        'https://feeds.bbci.co.uk/news/politics/rss.xml',
+        'https://rss.nytimes.com/services/xml/rss/nyt/Politics.xml',
+        'https://feeds.npr.org/1014/rss.xml',
+        'https://thehill.com/feed/',
+        'https://www.politico.com/rss/politicopicks.xml',
+        'https://feeds.washingtonpost.com/rss/politics'
+    ],
+    world: [
+        'https://feeds.bbci.co.uk/news/world/rss.xml',
+        'https://rss.nytimes.com/services/xml/rss/nyt/World.xml',
+        'https://www.aljazeera.com/xml/rss/all.xml',
+        'https://www.theguardian.com/world/rss',
+        'https://rss.dw.com/rdf/rss-en-world',
+        'https://www.france24.com/en/rss',
+        'https://feeds.skynews.com/feeds/rss/world.xml',
+        'https://www.euronews.com/rss?level=theme&name=news',
+        'https://abcnews.go.com/abcnews/internationalheadlines'
+    ],
+    crypto: [
+        'https://www.coindesk.com/arc/outboundfeeds/rss/',
+        'https://cointelegraph.com/rss',
+        'https://decrypt.co/feed',
+        'https://www.theblock.co/rss.xml',
+        'https://bitcoinmagazine.com/.rss/full/'
+    ],
+    business: [
+        'https://feeds.bbci.co.uk/news/business/rss.xml',
+        'https://rss.nytimes.com/services/xml/rss/nyt/Business.xml',
+        'https://feeds.bloomberg.com/markets/news.rss',
+        'https://www.cnbc.com/id/100003114/device/rss/rss.html',
+        'https://feeds.marketwatch.com/marketwatch/topstories/',
+        'https://www.investing.com/rss/news.rss',
+        'https://seekingalpha.com/feed.xml'
+    ],
+    tech: [
+        'https://feeds.arstechnica.com/arstechnica/index',
+        'https://www.theverge.com/rss/index.xml',
+        'https://techcrunch.com/feed/',
+        'https://www.wired.com/feed/rss',
+        'https://feeds.feedburner.com/TechCrunch/',
+        'https://www.engadget.com/rss.xml'
+    ],
+    sports: [
+        'https://www.espn.com/espn/rss/news',
+        'https://feeds.bbci.co.uk/sport/rss.xml',
+        'https://www.skysports.com/rss/12040',
+        'https://rss.nytimes.com/services/xml/rss/nyt/Sports.xml'
+    ],
+    esports: [
+        'https://www.hltv.org/rss/news',
+        'https://www.dexerto.com/feed/',
+        'https://www.gamesindustry.biz/feed',
+        'https://kotaku.com/rss'
+    ],
+    science: [
+        'https://www.sciencedaily.com/rss/all.xml',
+        'https://rss.nytimes.com/services/xml/rss/nyt/Science.xml',
+        'https://feeds.bbci.co.uk/news/science_and_environment/rss.xml',
+        'https://www.newscientist.com/feed/home/?cmpid=RSS|NSNS-Home',
+        'https://phys.org/rss-feed/',
+        'https://www.livescience.com/feeds/all'
+    ],
+    breaking: [
+        'https://feeds.bbci.co.uk/news/rss.xml',
+        'https://rss.cnn.com/rss/edition.rss',
+        'https://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml',
+        'https://abcnews.go.com/abcnews/topstories',
+        'https://feeds.nbcnews.com/nbcnews/public/news',
+        'https://www.cbsnews.com/latest/rss/main'
+    ]
+};
+
+module.exports = RSS_FEEDS;
